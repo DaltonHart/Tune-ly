3343bf58f2124c648613799680f5eb5d755a6134
Dalton Hart <DaltonVaren@gmail.com>
2019-07-19 14:50:57 -0700

    adds fix for exports with routes

--- controllers/songsCtrl.js
+++ controllers/songsCtrl.js
@@ -4,50 +4,61 @@ const router = express.Router();
 // Database
 const db = require('../models');
 
-
 // GET Songs Index
 router.get('/', (req, res) => {
-  db.Song.find({}, (err, allSongs) => {
-    if (err) return res.status(400).json({
-      status: 400, message: 'Something went wrong, please try again'
-    });
+	db.Song.find({}, (err, allSongs) => {
+		if (err)
+			return res.status(400).json({
+				status: 400,
+				message: 'Something went wrong, please try again'
+			});
 
-    res.json({ status: 200, data: allSongs });
-  });
+		res.json({ status: 200, data: allSongs });
+	});
 });
-
 
 // GET Songs Show
 router.get('/:cityId', (req, res) => {
-  db.Song.findById(req.params.cityId, (err, foundSong) => {
-    if (err) return res.status(400).json({
-      status: 400, message: 'Something went wrong, please try again'
-    });
+	db.Song.findById(req.params.cityId, (err, foundSong) => {
+		if (err)
+			return res.status(400).json({
+				status: 400,
+				message: 'Something went wrong, please try again'
+			});
 
-    res.json({ status: 200, data: foundSong });
-  });
+		res.json({ status: 200, data: foundSong });
+	});
 });
-
 
 // PUT Songs Update
 router.put('/:cityId', (req, res) => {
-  db.Song.findByIdAndUpdate(req.params.cityId, req.body, { new: true }, (err, newSong) => {
-    if (err) return res.status(400).json({
-      status: 400, message: 'Something went wrong, please try again'
-    });
+	db.Song.findByIdAndUpdate(
+		req.params.cityId,
+		req.body,
+		{ new: true },
+		(err, newSong) => {
+			if (err)
+				return res.status(400).json({
+					status: 400,
+					message: 'Something went wrong, please try again'
+				});
 
-    res.json({ status: 200, data: newSong });
-  });
+			res.json({ status: 200, data: newSong });
+		}
+	);
 });
-
 
 // Delete Songs Destroy
 router.post('/:cityId', (req, res) => {
-  db.Song.findByIdAndDelete(req.params.cityId, (err, deletedSong) => {
-    if (err) return res.status(400).json({
-      status: 400, message: 'Something went wrong, please try again'
-    });
+	db.Song.findByIdAndDelete(req.params.cityId, (err, deletedSong) => {
+		if (err)
+			return res.status(400).json({
+				status: 400,
+				message: 'Something went wrong, please try again'
+			});
 
-    res.json({ status: 200, message: 'success' });
-  });
+		res.json({ status: 200, message: 'success' });
+	});
 });
+
+module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,14 @@
+const mongoose = require('mongoose');
+mongoose.connect(
+	'mongodb://localhost:27017/tunely',
+	{
+		useNewUrlParser: true
+	},
+	() => {
+		console.log('Mongodb Connected');
+	}
+);
+
 module.exports = {
 	Song: require('./Song'),
 	Artist: require('./Artist')
