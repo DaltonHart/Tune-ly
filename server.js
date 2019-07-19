6eb7b4d6c6328f2068f7da2c5895a75c9f713b40
Dalton Hart <DaltonVaren@gmail.com>
2019-07-19 12:30:43 -0700

    adds serving of view files

--- server.js
+++ server.js
@@ -9,10 +9,15 @@ const PORT = process.env.PORT || 4000;
 
 // SECTION Middleware
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 // SECTION Routes
 app.get('/status', (req, res) => {
 	res.json({ status: 200, message: 'OK' });
+});
+
+app.get('/', (req, res) => {
+	res.sendFile(`${__dirname}/views/index.html`);
 });
 
 // SECTION Server Listener
