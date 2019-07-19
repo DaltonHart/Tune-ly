43acefb0aac6f6df1d4f92210abb58d801784f53
Dalton Hart <DaltonVaren@gmail.com>
2019-07-19 12:04:53 -0700

    adds init server setup

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log('We are live! 🎉');
+// SECTION  Modules
+const express = require('express');
+
+// SECTION Instanced Modules
+const app = express();
+
+// SECTION System Configuration Variables
+const PORT = process.env.PORT || 4000;
+
+// SECTION Middleware
+app.use(express.json());
+
+// SECTION Routes
+app.get('/status', (req, res) => {
+	res.json({ status: 200, message: 'OK' });
+});
+
+// SECTION Server Listener
+app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`);
+});
